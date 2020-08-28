62f9c43d6768e2ce0191ead0263d0af380c3c168
zanderhorstman <zanderhorstman@gmail.com>
2020-08-28 09:08:04 -0400

    1.0

---
+++ src/FinalChallenge.java
@@ -0,0 +1,27 @@
+/*
+
+
+OUTPUT:
+Enter 10 words: kiwi pineapple apple banana orange grapes watermelon cherry strawberry melon
+
+Original Order:
+kiwi pineapple apple banana orange grapes watermelon cherry strawberry melon
+
+Alphabetical Order:
+apple banana cherry grapes kiwi melon orange pineapple strawberry watermelon
+
+Reversed Alphabetical Order:
+watermelon strawberry pineapple orange melon kiwi grapes cherry banana apple
+
+Upper case array:
+APPLE BANANA CHERRY GRAPES KIWI MELON ORANGE PINEAPPLE STRAWBERRY WATERMELON
+*/
+/*
+PseudoCode:
+
+*/
+public class FinalChallenge {
+    public static void main(String[] args) {
+
+    }
+}
